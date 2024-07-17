836b6e7fa42c25e264062be3af179e21dbfe625e
junghwa0609 <hayan094666@gmail.com>
2024-07-17 13:10:24 +0900

    redis로컬에서 확인, index에 뉴스버튼 -> 뉴스확인가능

--- django_file/weather/urls.py
+++ django_file/weather/urls.py
@@ -1,8 +1,11 @@
-from django.contrib import admin
+# weather/urls.py
 from django.urls import path
-import weather.views
+from weather.view.redis import fetch_and_store_news, display_news, news_view
+from weather.view.index import index  # 새로 추가된 index view
 
 urlpatterns = [
-    path("", weather.views.mainpage, name="mainpage"),
-    path("news/", weather.views.news, name="news"),
+    path('fetch-news/', fetch_and_store_news, name='fetch_news'),
+    path('display-news/', display_news, name='display_news'),
+    path('', index, name='index'),  # 기본 경로 설정
+    path('news/', news_view, name='news'),
 ]
