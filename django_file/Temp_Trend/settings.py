49adbf6ca92c26eb3cccb63a60faed4f5eb53be2
junghwa0609 <hayan094666@gmail.com>
2024-07-18 14:04:05 +0900

    db변경 postgresql, admin custom
    migration해야함

--- django_file/Temp_Trend/settings.py
+++ django_file/Temp_Trend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Temp_Trend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'final_project',
+        'USER': 'truly_flower',
+        'PASSWORD': '090406',  # PostgreSQL 사용자 실제 비밀번호
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
