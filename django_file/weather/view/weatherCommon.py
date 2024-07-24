ea0a523ae7c37e43a0310cbd2b6e21e12210b6ec
poiup <dkswodud0531@gmail.com>
2024-07-24 17:23:46 +0900

    mutidb test

---
+++ django_file/weather/view/weatherCommon.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from datetime import datetime
+from models import WeatherData
+
+def we_data_test(request):
+    test = WeatherData.objects.filter(nx='33')
+    context = {'test' : test}
+    return render(request, 'wedatatest.html', context)
