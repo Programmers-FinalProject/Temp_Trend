77aee93c4f2087c5df13fb3fba2f45056735bb6c
caindy-P <akek1200@gmail.com>
2024-07-26 17:31:05 +0900

    redshift dag 완성

--- airflow/dag/mushinsa_to_redshift.py
+++ airflow/dag/mushinsa_to_redshift.py
@@ -4,12 +4,14 @@ from airflow.models import Variable
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 from airflow.utils.dates import days_ago
-import datetime
+from datetime import datetime
 
 # 기본 인수 설정
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 1, 1),
+    'depends_on_past': False,
+    'start_date': datetime(2024, 7, 24),
+    'catchup' : False,
     'retries': 1,
 }
 
@@ -18,13 +20,14 @@ dag = DAG(
     dag_id = 'mushinsa_to_redshift',
     default_args=default_args,
     schedule_interval='@daily',
+    max_active_runs=1,
 )
 
 # S3 버킷과 파일 정보
 S3_BUCKET = 'team-hori-1-bucket'
 S3_KEY = 'musinsa.csv'
 REDSHIFT_TABLE = 'musinsa'
-REDSHIFT_SCHEMA = 'dev'
+REDSHIFT_SCHEMA = 'musinsa'
 AWS_ACCESS_KEY_ID = Variable.get('ACCESS_KEY')
 AWS_SECRET_ACCESS_KEY = Variable.get('SECRET_KEY')
 
@@ -32,8 +35,12 @@ AWS_SECRET_ACCESS_KEY = Variable.get('SECRET_KEY')
 REDSHIFT_CONN_ID = 'redshift_default'
 
 # 테이블 드랍 및 생성 쿼리
-drop_create_table_sql = f"""
+drop_table_sql = f"""
     DROP TABLE IF EXISTS {REDSHIFT_SCHEMA}.{REDSHIFT_TABLE};
+"""
+
+# 테이블 생성 쿼리
+create_table_sql = f"""
     CREATE TABLE {REDSHIFT_SCHEMA}.{REDSHIFT_TABLE} (
         "product_name" TEXT,
         "product_link" TEXT,
@@ -47,21 +54,19 @@ drop_create_table_sql = f"""
     );
 """
 
-# Redshift COPY 명령어
-COPY_SQL = f"""
-    COPY {REDSHIFT_SCHEMA}.{REDSHIFT_TABLE}
-    FROM 's3://{S3_BUCKET}/{S3_KEY}'
-    CREDENTIALS 'aws_access_key_id={AWS_ACCESS_KEY_ID}-id;aws_secret_access_key={AWS_SECRET_ACCESS_KEY}'
-    CSV
-    IGNOREHEADER 1
-    DELIMITER ',';
-"""
-
-# 테이블 드랍 및 생성 작업 정의
-drop_create_table = PostgresOperator(
-    task_id='drop_create_table',
+# 테이블 드랍 작업 정의
+drop_table = PostgresOperator(
+    task_id='drop_table',
     postgres_conn_id=REDSHIFT_CONN_ID,
-    sql=drop_create_table_sql,
+    sql=drop_table_sql,
+    dag=dag,
+)
+
+# 테이블 생성 작업 정의
+create_table = PostgresOperator(
+    task_id='create_table',
+    postgres_conn_id=REDSHIFT_CONN_ID,
+    sql=create_table_sql,
     dag=dag,
 )
 
@@ -72,12 +77,11 @@ load_to_redshift = S3ToRedshiftOperator(
     table=REDSHIFT_TABLE,
     s3_bucket=S3_BUCKET,
     s3_key=S3_KEY,
-    copy_options=['CSV', 'IGNOREHEADER 1', 'DELIMITER \',\''],
+    copy_options=["CSV", "IGNOREHEADER 1", "DELIMITER ','"],
     aws_conn_id='MyS3Conn',  # AWS 연결 ID, Airflow에서 설정한 ID
     redshift_conn_id=REDSHIFT_CONN_ID,
-    sql=COPY_SQL,
     dag=dag,
 )
 
 # 작업 순서 정의
-drop_create_table >> load_to_redshift
+drop_table>> create_table >> load_to_redshift
