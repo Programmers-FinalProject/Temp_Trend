c22b959a2a3ceb78f24968d26c97bfa405dafa63
caindy-P <akek1200@gmail.com>
2024-07-29 15:44:05 +0900

    django 무신사 리스트 테스트

--- django_file/weather/urls.py
+++ django_file/weather/urls.py
@@ -5,6 +5,7 @@ from weather.view.index import weather_view  # 새로 추가된 index view
 from weather.view import weather_views
 from weather.tests import test
 from weather.view.save_location import save_location
+from weather.view.musinsa_views import musinsa_list
 
 urlpatterns = [
     path('fetch-news/', fetch_and_store_news, name='fetch_news'),
@@ -14,4 +15,5 @@ urlpatterns = [
     path('test/',test,name='test'),
     path('we-data-test/', weather_views.we_data_test, name='wedatatest'),
     path('save_location/', save_location, name='save_location'),
+    path('musinsa-test', musinsa_list, name='musinsa-test'),
 ]
