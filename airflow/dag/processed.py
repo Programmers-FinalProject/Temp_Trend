6d0c4172bf714cca9086439e183144aa02bca729
junghwa0609 <hayan094666@gmail.com>
2024-08-16 03:19:40 +0900

    dag update

---
+++ airflow/dag/processed.py
@@ -0,0 +1,134 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.utils.dates import days_ago
+import pandas as pd
+import numpy as np
+from io import StringIO
+from datetime import datetime, timedelta
+import pytz
+from airflow.sensors.external_task_sensor import ExternalTaskSensor  # ExternalTaskSensor를 임포트
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 8, 15, 14, 30, 0), 
+    'retries': 1,
+}
+
+with DAG(
+    dag_id='s3_data_preprocessing_and_upload',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+) as dag:
+    
+    wait_for_task = ExternalTaskSensor(
+        task_id = 'wait_for_dag', # 완료될 때 까지 기다릴 Task ID
+        external_dag_id = '29cm_s3_to_redshift', # 완료될 때 까지 기다릴 Dag ID
+        allowed_states = ['success'], # 완료될 때 까지 기다림
+        mode='reschedule',        # reschedule 모드로 설정
+        timeout=600,              # 10분 동안 조건 충족을 기다림
+        poke_interval = 60, # 60초에 한번씩 완료됐나 체크
+        execution_delta =  timedelta(minutes=30), # 동일한 시간에 스케줄링을 해야하는데 시간차이가 너무 날 경우 이를 통해 맞춰줄 수 있음. 
+        queue='queue1'
+    )
+
+    @task
+    def preprocess_data():
+        seoul_tz = pytz.timezone('Asia/Seoul')
+        seoul_now = datetime.now(seoul_tz) - timedelta(days=1)
+        today = seoul_now.strftime("%Y%m%d")
+
+        bucket_name = 'team-hori-1-bucket'
+        file_key = f'crawling/29cm_bestitem_{today}.csv'
+
+        s3_hook = S3Hook(aws_conn_id='MyS3Conn')
+
+        try:
+            # S3에서 객체 가져오기
+            response = s3_hook.get_key(file_key, bucket_name)
+            csv_content = response.get()['Body'].read().decode('utf-8')
+
+            # DataFrame 생성 및 전처리
+            df = pd.read_csv(StringIO(csv_content))
+            
+            # 전처리 작업
+            bag_only_mask = df['category1'].str.contains('가방')
+            df.loc[bag_only_mask & df['category2'].notna(), 'category3'] = df.loc[bag_only_mask & df['category2'].notna(), 'category2']
+            df.loc[bag_only_mask, 'category1'] = '아이템'
+            df.loc[bag_only_mask, 'category2'] = '가방'
+            
+            bag_only_mask = df['category1'].str.contains('액세서리')
+            df.loc[bag_only_mask, 'category1'] = '아이템'
+            
+            clothing_mask = df['category1'].str.contains('의류')
+            df.loc[clothing_mask, 'category1'] = df.loc[clothing_mask, 'category2']
+            df.loc[clothing_mask, 'category2'] = df.loc[clothing_mask, 'category3']
+            df['category1'] = df['category1'].str.replace('^(여성|남성)', '', regex=True)
+
+            overseas_brand_mask = df['category1'] == '해외브랜드'
+            df.loc[overseas_brand_mask, ['category1', 'category2']] = df.loc[overseas_brand_mask, ['category2', 'category2']].values
+
+            exclusive_mask = df['category1'] == 'EXCLUSIVE'
+            df.loc[exclusive_mask, 'category1'] = df.loc[exclusive_mask, 'category2']
+
+            mask_item = df['category1'].isin(['이너웨어', '홈웨어'])
+            df.loc[mask_item, 'category3'] = df.loc[mask_item, 'category2']
+            df.loc[mask_item, 'category2'] = df.loc[mask_item, 'category1']
+            df.loc[mask_item, 'category1'] = '아이템'
+
+            mask_bottom = df['category1'].isin(['바지', '스커트'])
+            df.loc[mask_bottom, 'category3'] = df.loc[mask_bottom, 'category2']
+            df.loc[mask_bottom, 'category2'] = df.loc[mask_bottom, 'category1']
+            df.loc[mask_bottom, 'category1'] = '하의'
+
+            mask_top = df['category1'].isin(['원피스', '점프수트', '액티브웨어', '아우터', '니트웨어', '셋업'])
+            df.loc[mask_top, 'category3'] = df.loc[mask_top, 'category2']
+            df.loc[mask_top, 'category2'] = df.loc[mask_top, 'category1']
+            df.loc[mask_top, 'category1'] = '상의'
+
+            df.dropna(subset=['category1'], inplace=True)
+            df.loc[df['category2'] == df['category3'], 'category3'] = np.nan
+            df['category2'] = df['category2'] + df['category3'].apply(lambda x: f"/{x}" if pd.notna(x) else "")
+            df['category2'] = df['category2'].fillna(df['category1'])
+            df['gender'] = df['gender'].replace({'men': 'm', 'women': 'w'})
+
+            # df2: 필요한 열만 선택
+            df2 = df[['category1', 'category2', 'gender']].drop_duplicates(subset=['category1', 'category2', 'gender']).reset_index(drop=True)
+
+            # df: 모든 열이 포함된 데이터프레임에서 category1, category3을 제외한 나머지 열 선택
+            df_full = df.drop(columns=['category1', 'category3'])
+
+            # df_full에서 category2 열의 이름을 category로 변경
+            df_full = df_full.rename(columns={'category2': 'category'})
+
+
+            return {"df2": df2, "df_full": df_full}
+
+        except Exception as e:
+            raise ValueError(f"CSV 파일을 읽는 중 오류가 발생했습니다: {e}")
+
+    @task
+    def upload_df_to_s3(data,key,s3_key):
+        csv_buffer = StringIO()
+        df = data[key]
+        df.to_csv(csv_buffer, index=False)
+
+        s3_hook = S3Hook(aws_conn_id='my_amazon_conn')
+        s3_hook.load_string(csv_buffer.getvalue(), key=s3_key, bucket_name='team-hori-1-bucket', replace=True)
+
+    # Task 실행
+    data = preprocess_data()
+
+    seoul_tz = pytz.timezone('Asia/Seoul')
+    seoul_now = datetime.now(seoul_tz)
+    today = seoul_now.strftime("%Y%m%d")
+
+    upload_df_1 = upload_df_to_s3(data,"df2", f'model/file29/df2_{today}.csv')
+    upload_df_2 = upload_df_to_s3(data,"df_full", f'model/full29_processed/df_full_{today}.csv')
+    
+    wait_for_task >> data >> [upload_df_1, upload_df_2]
+
+dag_instance = dag
+
---
+++ airflow/dag/redis.py
@@ -0,0 +1,157 @@
+from airflow.decorators import dag, task
+from airflow.models import Variable
+from airflow.utils.dates import days_ago
+import requests
+import pandas as pd
+from io import StringIO
+import re
+import urllib.parse
+import html
+from bs4 import BeautifulSoup
+from datetime import datetime
+import pytz
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.amazon.aws.hooks.redshift_sql import RedshiftSQLHook
+from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
+
+def decode_html_entities(text):
+    return html.unescape(text)
+
+def remove_html_tags(text):
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
+def get_image_from_url(news_url):
+    try:
+        html_content = requests.get(news_url).text
+        soup = BeautifulSoup(html_content, "html.parser")
+        meta_og_image = soup.find("meta", property="og:image")
+        if meta_og_image:
+            return meta_og_image["content"]
+        else:
+            return "not found -> meta tag og:image"
+    except Exception as e:
+        print(f"get_meta_og_image | error: {e}")
+        return None
+
+# 기본 인자 설정
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+    'retries': 0,
+}
+
+@dag(default_args=default_args, schedule_interval='0 * * * *', catchup=False, dag_id='upload_dataframe_direct_to_s3')
+def upload_dag():
+    
+    @task
+    def fetch_and_store_news():
+        # Airflow 환경변수에서 값 가져오기
+        client_id = Variable.get("naver-client-id")
+        client_secret = Variable.get("naver-client-secret")
+
+        if not client_id or not client_secret:
+            raise ValueError("Client ID or Client Secret not found in Airflow variables")
+    
+        encText = urllib.parse.quote("날씨")
+        sorting = "&display=100&start=1&sort=sim"
+        url = "https://openapi.naver.com/v1/search/news?query=" + encText + sorting
+
+        headers = {
+            'X-Naver-Client-Id': client_id,
+            'X-Naver-Client-Secret': client_secret,
+        }
+
+        response = requests.get(url, headers=headers)
+    
+        if response.status_code == 200:
+            data = response.json()
+            items = data['items']
+            plus_data = []
+            for idx, item in enumerate(items):
+                # 키를 고유 ID로 설정. 1부터 시작
+                item_id = f"news:{idx+1}"
+                clean_title = decode_html_entities(remove_html_tags(item['title']))
+                clean_description = decode_html_entities(remove_html_tags(item['description']))
+                image_url = get_image_from_url(item['link'])
+                
+                context = {
+                    "id" : item_id,
+                    "title": clean_title,
+                    "description": clean_description,
+                    "link": item['link'],
+                    "pubDate": item['pubDate'],
+                    "image_url": image_url if image_url else ""
+                }
+                plus_data.append(context)
+        
+            df = pd.DataFrame(plus_data)
+            return df
+        else:
+            raise ValueError("Failed to fetch news data from API")
+    @task
+    def create_redshift_table():
+        redshift_hook = RedshiftSQLHook(redshift_conn_id='my_redshift_conn')
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS raw_data.news_weather_table (
+            title VARCHAR(65535),
+            description VARCHAR(65535),
+            link VARCHAR(2048),
+            pubDate VARCHAR(2048),
+            image_url VARCHAR(2048)
+        );
+        """
+        redshift_hook.run(create_table_query)
+        print("Redshift table created successfully.")
+
+    @task
+    def s3_upload(df):
+        if df is not None and not df.empty:
+            csv_buffer = StringIO()
+            df.to_csv(csv_buffer, index=False)
+
+            aws_hook = S3Hook(aws_conn_id='my_amazon_conn')
+            bucket_name = "team-hori-1-bucket"
+            
+            korea_timezone = pytz.timezone("Asia/Seoul")
+            current_time = datetime.now(korea_timezone)
+            formatted_time = current_time.strftime("%Y%m%d")
+            
+            s3_key = f'news/news_{formatted_time}.csv'
+            
+            aws_hook.load_string(
+                string_data=csv_buffer.getvalue(),
+                key=s3_key,
+                bucket_name=bucket_name,
+                replace=True
+            )
+
+            print(f"Dataframe uploaded to S3 successfully: {s3_key}")
+            return s3_key
+        else:
+            raise ValueError("Dataframe is empty or None")
+
+    
+    @task
+    def s3_to_redshift_append(s3_key):
+        s3_to_redshift = S3ToRedshiftOperator(
+            task_id="s3_to_redshift",
+            s3_bucket="team-hori-1-bucket",
+            s3_key=s3_key,
+            schema="raw_data",
+            table="news_weather_table",
+            copy_options=['csv', 'IGNOREHEADER 1'],
+            aws_conn_id="my_amazon_conn",
+            redshift_conn_id="my_redshift_conn"
+        )
+
+    # Task 인스턴스 생성 및 연결
+    create_table = create_redshift_table()
+    news_df = fetch_and_store_news()
+    s3_key = s3_upload(news_df)
+    s3_to_redshift_task = s3_to_redshift_append(s3_key)
+
+    create_table >> news_df >> s3_key >> s3_to_redshift_task
+
+# DAG 인스턴스 생성
+dag_instance = upload_dag()
